faed17a6c52c6667a8a1a48484bdd4c098ca3e80
Ricky-Hu5918 <31519963@qq.com>
2019-12-09 15:57:02 +0800

    commit addDigits.py file for the 2st round using recursion to address this problem

--- addDigits.py
+++ addDigits.py
@@ -2,52 +2,20 @@
 # -*- coding: UTF-8 -*-
 
 #Leetcode.num = 258
-""""------1st round------
-#最low的版本，使用了多次重复循环。先把数字拆开，放入列表，然后计算列表中数值之和，
-# 若和大于10则继续拆，连续拆三次（一般三次拆分之后数字之和必定为个位数）
+""""------2st round------
+使用递归！！！
+正整数拆分并相加，将相加后的新数字继续拆分和相加，直到最后相加的数字小于10，即为返回结果
 """
 def addDigits(num):
-    A = []
-    B = []
-
     if (num < 10):
         return num
-    else:
-        while num >= 10:
-            A.append(num % 10)
-            num = int(num / 10)
-        A.append(num)
-    # print(A)
 
-    num = 0
-    for each in A:
-        num += each
-    if num < 10:
-        return num
-    else:
-        while num >= 10:
-            B.append(num % 10)
-            num = int(num / 10)
-        B.append(num)
-    # print(B)
+    new_num = 0
+    while (num != 0):
+        new_num += (num % 10)
+        num = int(num / 10)
 
-    num = 0
-    for i in B:
-        num += i
-    if num < 10:
-        return num
-    else:
-        A = []
-        while num >= 10:
-            A.append(num % 10)
-            num = int(num / 10)
-        A.append(num)
-
-    num = 0
-    for i in A:
-        num += i
-
-    return num
+    return addDigits(new_num)
 
 if __name__ == "__main__":
     num = 199
