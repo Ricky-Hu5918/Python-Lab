dfaa067bb8e892beee81f7d346e1215ec9809bf4
Ricky-Hu5918 <31519963@qq.com>
2019-12-16 14:48:08 +0800

    commit the 3rd version, the combination one.

--- balancedparenthes.py
+++ balancedparenthes.py
@@ -18,24 +18,24 @@ Output : Unbalanced
 如果是右括弧，则检查stack是否为空，若为空则为Unbalanced，或者检查pop出stack的字符是否为对应的左括弧，若不是，则为Unbalanced；
 最后检查valid标志和stack是否为空
 '''
+'''
+#3rd version：结合肖哥的思路和上一个stack的思路的综合版本。
+'''
 def balanced_parentheses(str):
     valid = True
-    stack = []
+    detect_stack = []
+    left_parentheses = "([{"
+    right_parentheses = ")]}"
+    parentheses_dict = {'(':')', '[':']', '{':'}'}
 
     for each in str:
-        if ((each=='(') or (each=='[' ) or (each=='{')):
-            stack.append(each)
-        elif each==')':
-            if not stack or (stack.pop() != '('):
-                valid = False
-        elif each==']':
-            if not stack or (stack.pop() != '['):
-                valid = False
-        elif each=='}':
-            if not stack or (stack.pop() != '{'):
+        if (each in left_parentheses):
+            detect_stack.append(each)
+        elif each in right_parentheses:
+            if not detect_stack or (each != parentheses_dict[detect_stack.pop()]):
                 valid = False
 
-    if not stack and (valid):
+    if not detect_stack and (valid):
         return "Balanced"
     else:
         return "Unbalanced"
@@ -49,5 +49,6 @@ arr4 = '({)}'           # unbalanced
 arr5 = '[)'             # unbalanced
 arr6 = '{{{[]}}}}'      # unbalanced
 arr7 = '{{{[]}}}([)][[()){{}]}'      # unbalanced
-print(balanced_parentheses(arr7))
+arr8 = '))))'
+print(balanced_parentheses(arr8))
 
