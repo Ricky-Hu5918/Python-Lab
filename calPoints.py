2ef9fd7e0b2d43dbaa442c6f4b0fd26335cdfb61
Ricky-Hu5918 <31519963@qq.com>
2019-12-26 12:55:47 +0800

    commit calPoints.py with the 'stack' version.

---
+++ calPoints.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#Leetcode.num = 682. Baseball Game
+"""
+You're now a baseball game point recorder.
+
+Given a list of strings, each string can be one of the 4 following types:
+Integer (one round's score): Directly represents the number of points you get in this round.
+"+" (one round's score): Represents that the points you get in this round are the sum of the last two valid round's points.
+"D" (one round's score): Represents that the points you get in this round are the doubled data of the last valid round's points.
+"C" (an operation, which isn't a round's score): Represents the last valid round's points you get were invalid and should be removed.
+Each round's operation is permanent and could have an impact on the round before and the round after.
+
+You need to return the sum of the points you could get in all the rounds.
+
+Example 1:
+Input: ["5","2","C","D","+"]
+Output: 30
+Explanation:
+Round 1: You could get 5 points. The sum is: 5.
+Round 2: You could get 2 points. The sum is: 7.
+Operation 1: The round 2's data was invalid. The sum is: 5.
+Round 3: You could get 10 points (the round 2's data has been removed). The sum is: 15.
+Round 4: You could get 5 + 10 = 15 points. The sum is: 30.
+"""
+def calPoints(ops):
+    scores_stack = []
+
+    for i in range(len(ops)):
+        if (ops[i] not in ['C', 'D', '+']):
+            scores_stack.append(int(ops[i]))
+        elif (ops[i] == 'C'):
+            scores_stack.pop()
+        elif (ops[i] == 'D'):
+            scores_stack.append(scores_stack[-1]*2)
+        else:
+            scores_stack.append(sum(scores_stack[-2:]))  #(scores_stack[-1]+scores_stack[-2])
+
+    return sum(scores_stack[:])
+
+ops = ["5","-2","4","C","D","9","+","+"] #output = 27
+ops2 = ["5","2","C","D","+"] #output = 30
+print(calPoints(ops))
