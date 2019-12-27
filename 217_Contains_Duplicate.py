da7b3919727127d93285f3d3d58d4f98586007d6
Ricky-Hu5918 <31519963@qq.com>
2019-12-27 22:22:17 +0800

    commit a much better version of xk.

--- 217_Contains_Duplicate.py
+++ 217_Contains_Duplicate.py
@@ -26,11 +26,25 @@ def containsDuplicate2(nums):
 ''' #3: using Counter() method. 140ms'''
 from collections import Counter
 def containsDuplicate3(nums):
-    c = Counter(nums)
-    for each in list(c.values()):
-        if (each > 1):
+    #c = Counter(nums)
+    #for each in list(c.values()):
+    for k, v in Counter(nums).items():
+        if (v > 1):
             return True
     return False
+
+''' #3+: xk's much better version '''
+from collections import Counter
+def containsDuplicate33(nums):
+    dict_nums = {}
+    for item in nums:  #功能类似Counter，生成一个字典，key是列表元素，value是元素出现的次数
+        dict_nums[item] = dict_nums.get(item, 0) + 1
+
+    for k, v in dict_nums.items():
+        if (v > 1):
+            return True
+    return False
+
 
 ''' #4: timeout'''
 def containsDuplicate4(nums):
@@ -62,5 +76,5 @@ def containsDuplicate6(nums):
 
 nums = [1,2,3,1]
 nums1 = [1]
-nums2 = [1,2,3]
-print(containsDuplicate3(nums2))
+nums2 = [1,2,3,2,2,2]
+print(containsDuplicate33(nums2))
