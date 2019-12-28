5d332869b381e9afdcd4cbe8b480816ccea47bbb
Ricky-Hu5918 <31519963@qq.com>
2019-12-28 14:42:22 +0800

    commit 350_Intersection_of_Two_Arrays_II.py with xk's versions.

--- 350_Intersection_of_Two_Arrays_II.py
+++ 350_Intersection_of_Two_Arrays_II.py
@@ -51,8 +51,26 @@ def intersect3(nums1, nums2):
 
     return res
 
+'''#4: xk's method. Perfect!!!'''
+def intersect4(nums1, nums2):
+    dict_nums1, dict_nums2, res = {}, {}, []
+
+    for item in nums1:  #将nums1中的元素与其出现次数组成字典
+        dict_nums1[item] = dict_nums1.get(item, 0) + 1
+
+    for item in nums2:  #将nums2中的元素与其出现次数组成字典
+        dict_nums2[item] = dict_nums2.get(item, 0) + 1
+
+    for k, v in dict_nums1.items(): #遍历nums1的字典
+        if dict_nums2.get(k):  #如果nums1字典中的元素在nums2的字典中，则.get()返回其在nums2字典中的次数，否则返回0
+            for i in range(min(v, dict_nums2.get(k))): #将同时存在于两个字典中的元素，按最小的重复次数添加到返回列表
+                res.append(k)
+
+    return res
+
+
 nums1, nums2 = [1,2,2,1], [2,2]
 nums3, nums4 = [4,9,5], [9,4,9,8,4]
 nums5 = [54,93,21,73,84,60,18,62,59,89,83,89,25,39,41,55,78,27,65,82,94,61,12,38,76,5,35,6,51,48,61,0,47,60,84,9,13,28,38,21,55,37,4,67,64,86,45,33,41]
 nums6 = [17,17,87,98,18,53,2,69,74,73,20,85,59,89,84,91,84,34,44,48,20,42,68,84,8,54,66,62,69,52,67,27,87,49,92,14,92,53,22,90,60,14,8,71,0,61,94,1,22,84,10,55,55,60,98,76,27,35,84,28,4,2,9,44,86,12,17,89,35,68,17,41,21,65,59,86,42,53,0,33,80,20]
-print(intersect3(nums5, nums6))
+print(intersect4(nums5, nums6))
