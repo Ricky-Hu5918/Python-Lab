13fda939ef9c53491955afee4783d4e473fadb99
Ricky-Hu5918 <31519963@qq.com>
2020-01-02 17:54:35 +0800

    commit 299_Bulls_and_Cows.py file with xk's better version.

--- 299_Bulls_and_Cows.py
+++ 299_Bulls_and_Cows.py
@@ -24,18 +24,17 @@ Note: You may assume that the secret number and your friend's guess only contain
 """
 
 '''#1: 匹配的数字（bulls）要移除，不匹配的数字也需要移除，这样才能去重。'''
-def getHint(secret, guess):
-    bulls_count, cows_count, i, j = 0, 0, 0, 0
+def getHint1(secret, guess):
+    bulls_count, cows_count, i = 0, 0, 0
     bulls_idx = []
 
     ll = len(guess)
 
     while (ll != 0):
-        if (guess[i] == secret[j]):
+        if (guess[i] == secret[i]):
             bulls_count += 1
             bulls_idx.append(i)
         i += 1
-        j += 1
         ll -= 1
 
     secret_list = list(secret)
@@ -49,12 +48,34 @@ def getHint(secret, guess):
     for item in (guess_list):
         if (item in secret_list):
             cows_count += 1
-            secret_list.remove(item)
+            secret_list.remove(item)  #去重处理
 
     return str(bulls_count)+'A'+str(cows_count)+'B'
+
+'''#2: 肖哥的版本，学习思路和处理方法'''
+def getHint2(secret, guess):
+    bulls_count, cows_count = 0, 0
+    non_bulls_secret_list, non_bulls_guess_list = [], []
+
+    #下面这段写的很精妙
+    for i in range(len(secret)):
+        if (secret[i] == guess[i]):  #对应相等则计数为bulls
+            bulls_count += 1
+        else:  #对应不等，则将这些元素分别放进list中
+            non_bulls_secret_list.append(secret[i])
+            non_bulls_guess_list.append(guess[i])
+
+    print(non_bulls_guess_list,non_bulls_secret_list)
+    #处理list中cows的部分，注意要做去重处理
+    for item in set(non_bulls_guess_list):
+        cows_count += min(non_bulls_secret_list.count(item), non_bulls_guess_list.count(item))
+
+    return str(bulls_count)+'A'+str(cows_count)+'B'
+
 
 secret, guess = "1807", "7810"  #"1A3B"
 secret1, guess1 = "1123", "0111"  #1A1B
 secret2, guess2 = "1122", "2211"  #"0A4B"
-secret3, guess3 = "11", "11"
-print(getHint(secret, guess))
+secret3, guess3 = "11", "11"  #"2A0B"
+secret4, guess4 = "1122", "0001"  #"0A1B"
+print(getHint2(secret, guess))
