d207302794ea7511321fd166a8fc59df228dea6c
Ricky-Hu5918 <31519963@qq.com>
2020-01-09 12:10:15 +0800

    commit 1287_Find_Special_Integer.py file with 4 versions.

---
+++ 1287_Find_Special_Integer.py
@@ -0,0 +1,59 @@
+"""
+Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time.
+Return that integer.
+
+Example 1:
+
+Input: arr = [1,2,2,6,6,6,6,7,10]
+Output: 6
+ 
+Constraints:
+1 <= arr.length <= 10^4
+0 <= arr[i] <= 10^5
+"""
+'''#1: 常规方法，使用count'''
+def findSpecialInteger1(arr):
+    ll = len(arr)
+    for item in arr:
+        if (arr.count(item)*4 > ll):
+            return item
+
+'''#2: 常规方法，使用Counter'''
+from collections import Counter
+def findSpecialInteger2(arr):
+    for k, v in Counter(arr).items():
+        if (v*4 > len(arr)):
+            return k
+
+'''#3: 常规方法，自己用字典造一个元素出现次数统计表'''
+def findSpecialInteger3(arr):
+    dict_arr = {}
+
+    for item in arr:
+        dict_arr[item] = dict_arr.get(item, 0) + 1
+
+    for k, v in dict_arr.items():
+        if (v*4 > len(arr)):
+            return k
+
+'''#4: 按照len/4的步长进行搜索'''
+import math
+def findSpecialInteger4(arr):
+    ll = len(arr)
+    if (ll == 1):
+        return arr[0]
+
+    search_step = 1 if (not math.ceil(ll/4)) else math.ceil(ll/4)
+    idx = 0
+    while (idx+search_step < ll):
+        if (arr[idx] == arr[idx+search_step]):
+            return arr[idx]
+        else:
+            idx += 1
+
+    return arr[0]
+
+arr = [1]
+arr1 = [1,2,2,6,6,6,6,7,10]
+arr2 = [9057,13452,13452,13452,13452,13452,14141,14448,60395,95081]
+print(findSpecialInteger1(arr2))
