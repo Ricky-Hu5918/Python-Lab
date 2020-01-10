bcdb2638a1281a75eff79560e8b5fe36f2e3b196
Ricky-Hu5918 <31519963@qq.com>
2020-01-10 22:49:13 +0800

    commit 28_Implement_strStr().py with a better version.

--- 28_Implement_strStr().py
+++ 28_Implement_strStr().py
@@ -36,6 +36,40 @@ def strStr(haystack, needle):
         if (i == len_haystack - 1):
             return -1
 
+'''#2: 代码长度增加，但是效率有所提高'''
+def strStr2(haystack, needle):
+    if not needle:
+        return 0
+
+    if not haystack:
+        return -1
+
+    len_needle, len_haystack = len(needle), len(haystack)
+    if (len_needle > len_haystack):
+        return -1
+
+    idx, count = 0, 0
+    while (idx < len_haystack):
+        if (haystack[idx] == needle[0]):
+            if (len_haystack-1-idx >= len_needle-1):
+                for i in range(1, len_needle):
+                    if (haystack[idx+i] == needle[i]):
+                        count += 1
+                    else:
+                        count = 0
+                        break
+
+                if (count+1 == len_needle):
+                    return idx
+            else:
+                return -1
+        idx += 1
+
+    return -1
+
+
 haystack, needle = "hello", "ll"
-haystack1, needle1 = "aaaa", "aa"
-print(strStr(haystack, needle))
+haystack1, needle1 = "aaaba", "baa"
+haystack2, needle2 = "babba", "bba"
+haystack3, needle3 = "babaa", "baa"
+print(strStr2(haystack3, needle3))
