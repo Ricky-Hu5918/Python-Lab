4197d708a35c32ba8a77eed2ab680c5a3f77bc1c
Ricky-Hu5918 <31519963@qq.com>
2020-01-10 22:13:23 +0800

    commit 28_Implement_strStr().py with 1 version.

---
+++ 28_Implement_strStr().py
@@ -0,0 +1,41 @@
+"""
+Implement strStr().
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+Input: haystack = "hello", needle = "ll"
+Output: 2
+
+Example 2:
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+
+Clarification:
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+For the purpose of this problem, we will return 0 when needle is an empty string.
+This is consistent to C's strstr() and Java's indexOf().
+"""
+
+'''#1: 常规方法，先找到needle的第一个元素，然后切片比较是否等于needle'''
+def strStr(haystack, needle):
+    if not needle:
+        return 0
+
+    if not haystack:
+        return -1
+
+    len_needle, len_haystack = len(needle), len(haystack)
+    if (len_needle > len_haystack):
+        return -1
+
+    for i in range(len_haystack):
+        if (needle[0] == haystack[i]):
+            if (haystack[i:(i + len_needle)] == needle):
+                return i
+
+        if (i == len_haystack - 1):
+            return -1
+
+haystack, needle = "hello", "ll"
+haystack1, needle1 = "aaaa", "aa"
+print(strStr(haystack, needle))
