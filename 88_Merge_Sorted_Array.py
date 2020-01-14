9ed4429e9450b9be88c16ed9a0c17de3be8a62dc
Ricky-Hu5918 <31519963@qq.com>
2020-01-14 12:05:34 +0800

    commit 88_Merge_Sorted_Array.py file with a bug fixed version.

--- 88_Merge_Sorted_Array.py
+++ 88_Merge_Sorted_Array.py
@@ -26,8 +26,8 @@ def merge(nums1, m, nums2, n):
     for i in range(n): #遍历nums2，将其插入nums1中，并计数
         count += insert_element(nums1, nums2[i])  #累加插入次数
 
-    for count in range(count, n): #nums2中剩余没有插入nums1的数，说明都比nums1中大，需要从m+count位置直接插入nums1
-        nums1.insert(m+count, nums2[count])
+    for i in range(count, n): #nums2中剩余没有插入nums1的数，说明都比nums1中大，需要从m+count位置直接插入nums1
+        nums1.insert(m+i, nums2[i])
 
     for i in range(len(nums1)-1, -1, -1): #nums1中补0的元素倒序剔除，但要注意最终nums1的长度必须等于m+n
         if (nums1[i] == 0) and (i>=m+n):
@@ -37,8 +37,8 @@ def merge(nums1, m, nums2, n):
 
     return nums1
 
-nums1, nums2 = [1,2,0,0,0], [2,5,6]  #[1,2,2,5,6]
-m, n= 2, 3
+nums1, nums2 = [1,2,3,0,0,0], [2,5,6]  #[1,2,2,3,5,6]
+m, n= 3, 3
 
 nums3, nums4 = [-1,0,0,3,3,3,0,0,0], [1,2,2]  #[-1, 0, 0, 1, 2, 2, 3, 3, 3]
 m3, n3 = 6, 3
@@ -46,4 +46,4 @@ m3, n3 = 6, 3
 nums5, nums6 = [-1,-1,0,0,0,0], [-1,0]
 m4, n4 = 4, 2
 
-print(merge(nums5, m4, nums6, n4))
+print(merge(nums1, m, nums2, n))
