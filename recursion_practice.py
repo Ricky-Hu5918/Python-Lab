d26194169b688be592fe6bca33318e9d516e9554
Ricky-Hu5918 <31519963@qq.com>
2020-01-30 16:34:08 +0800

    commit quick sort algorithm.

--- recursion_practice.py
+++ recursion_practice.py
@@ -1,8 +1,9 @@
 '''for recursion practice'''
+'''递归recursion的前提是：找到基准条件base case和递归条件recursion case'''
 
 '''#1: calculate the sum of an arr'''
 def sum(arr):
-    if not arr:
+    if not arr:  #arr==None is the base case.
         return 0
     else:
         return arr[0]+sum(arr[1:])
@@ -23,6 +24,18 @@ def find_largestItem(arr):
             arr[1] = arr[0]
         return find_largestItem(arr[1:])
 
+'''#4: quick sort algorithm'''
+'''steps: a选择基准值pivot，b对数组进行分区，分为比pivot大和小的两个区，c对这两个分区进行快速排序（递归）'''
+def quick_sort(arr):
+    if (len(arr)<2):
+        return arr  #base case
+    else:
+        pivot = arr[0]  #recursion case
+        small_items = [item for item in arr[1:] if item<pivot]
+        large_items = [item for item in arr[1:] if item>=pivot]
 
-arr = [7, 2, 3, 4, 6]
-print(find_largestItem(arr))
+        return (quick_sort(small_items)) + [pivot] + (quick_sort(large_items)) #ascending
+        #return (quick_sort(large_items)) + [pivot] + (quick_sort(small_items)) #descending
+
+arr = [1, 7, 2, 3, 0, 4, 6, -1, -3]
+print(quick_sort(arr))
