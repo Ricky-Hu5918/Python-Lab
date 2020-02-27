ad167441fdff43ca294e681056e97143ea0b796f
Ricky-Hu5918 <31519963@qq.com>
2020-02-27 21:58:56 +0800

    commit 21_Merge_Two_Sorted_Lists.py with other 2 better versions.

--- 21_Merge_Two_Sorted_Lists.py
+++ 21_Merge_Two_Sorted_Lists.py
@@ -48,4 +48,40 @@ def mergeTwoLists(l1, l2):
     return mergedlists
 
 
+'''#2: 迭代版本'''
+def mergeTwoLists2(l1, l2):
+    if not l1:
+        return l2
+    elif not l2:
+        return l1
+    else:
+        if l1.val < l2.val:
+            l1.next = mergeTwoLists2(l1.next, l2) #self.mergeTwoLists2(l1.next, l2)
+            return l1
+        else:
+            l2.next = mergeTwoLists2(l2.next, l2) #self.mergeTwoLists2(l2.next, l2)
+            return l2
+
+'''#3: 递归版本'''
+def mergeTwoLists3(l1, l2):
+    prehead = ListNode(-1)
+    pre = prehead
+
+    while (l1 and l2):
+        if l1.val < l2.val:
+            pre.next = l1
+            l1 = l1.next
+        else:
+            pre.next = l2
+            l2 = l2.next
+
+        pre = pre.next
+
+    pre.next = l1 if l1 is not None else l2
+
+    return prehead.next
+
+
+
+
 
