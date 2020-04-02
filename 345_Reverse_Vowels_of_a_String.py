60c4cdec8f56e5b96498f31cfcfd0590a11a515b
Ricky-Hu5918 <31519963@qq.com>
2020-04-02 18:47:49 +0800

    commit 345_Reverse_Vowels_of_a_String.py with 2 versions.

---
+++ 345_Reverse_Vowels_of_a_String.py
@@ -0,0 +1,60 @@
+'''
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+Input: "hello"
+Output: "holle"
+
+Example 2:
+Input: "leetcode"
+Output: "leotcede"
+Note:
+The vowels does not include the letter "y".
+'''
+'''#1: 先找到vowel元素在字符串中的index，然后操作字符串进行前后调换顺序'''
+def reverseVowels1(s):
+    list_vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    reverse_idx = []
+
+    for i, j in enumerate(s): #find idx of vowels elements
+        if j in list_vowels:
+            reverse_idx.append(i)
+
+    if not reverse_idx: return s
+
+    list_s = list(s)
+    ll = len(reverse_idx)
+    tmp = (ll//2)
+    idx = 0
+    while (tmp != 0):
+        list_s[reverse_idx[idx]], list_s[reverse_idx[ll-1-idx]] = \
+            list_s[reverse_idx[ll-1-idx]], list_s[reverse_idx[idx]]
+        idx += 1
+        tmp -= 1
+
+    return ''.join(list_s)
+
+
+'''#2: two point'''
+def reverseVowels2(s):
+    list_vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    list_s = list(s)
+    left_idx, right_idx = 0, len(s) - 1
+
+    while (left_idx <= right_idx):
+        if (list_s[left_idx] not in list_vowels):
+            left_idx += 1
+
+        if (list_s[right_idx] not in list_vowels):
+            right_idx -= 1
+
+        if (list_s[left_idx] in list_vowels) and (list_s[right_idx] in list_vowels):
+            list_s[left_idx], list_s[right_idx] = list_s[right_idx], list_s[left_idx]
+            left_idx += 1
+            right_idx -= 1
+
+    return ''.join(list_s)
+
+s1 = 'aA'
+s2 = 'leetcoad'
+print(reverseVowels1(s1))
