5a6300538020f4969c66176eb8b1f5a172537aac
Ricky-Hu5918 <31519963@qq.com>
2020-04-07 14:59:40 +0800

    commit 225_Implement_Stack_using_Queues.py with 1 version.

---
+++ 225_Implement_Stack_using_Queues.py
@@ -0,0 +1,70 @@
+'''
+Implement the following operations of a stack using queues.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+empty() -- Return whether the stack is empty.
+
+Example:
+MyStack stack = new MyStack();
+stack.push(1);
+stack.push(2);
+stack.top();   // returns 2
+stack.pop();   // returns 2
+stack.empty(); // returns false
+
+Notes:
+You must use only standard operations of a queue -- which means only push to back, peek/pop from front, size, and is empty operations are valid.
+Depending on your language, queue may not be supported natively. You may simulate a queue by using a list or deque (double-ended queue), as long as you use only standard operations of a queue.
+You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
+'''
+
+from collections import deque
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.dq = deque()
+        self.size = 0
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.dq.append(x)
+        self.size += 1
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        if (not self.empty()):
+            self.size -= 1
+            return self.dq.pop()
+        else:
+            return -1
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        if (not self.empty()):
+            return self.dq[-1]
+        else:
+            return -1
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return (self.size == 0)
+
+stack = MyStack()
+print(stack.push(1))
+print(stack.push(2))
+print(stack.top())
+print(stack.pop())
+print(stack.empty())
