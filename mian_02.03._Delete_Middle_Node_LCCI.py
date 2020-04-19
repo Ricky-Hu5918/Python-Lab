587637efeecf8e7a09e65d54175e51bf7aaa23aa
Ricky-Hu5918 <31519963@qq.com>
2020-04-19 12:58:05 +0800

    commit mian_02.03._Delete_Middle_Node_LCCI.py with 2 versions.

--- mian_02.03._Delete_Middle_Node_LCCI.py
+++ mian_02.03._Delete_Middle_Node_LCCI.py
@@ -25,7 +25,7 @@ class Solution1:
         node.val = node.next.val
         node.next = node.next.next
 
-'''给定了链表和要删除的节点'''
+'''给定了链表和要删除的节点，要删除的节点不包含头节点和尾节点'''
 class Solution2:
     def deleteNode2(self, node: ListNode, n: int) -> None:
         """
@@ -41,3 +41,60 @@ class Solution2:
                 break
             else:
                 cur = cur.next
+
+'''#面试题18. 删除链表的节点(节点包含头、尾节点)
+给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+
+返回删除后的链表的头节点。
+
+注意：此题对比原题有改动
+
+示例 1:
+
+输入: head = [4,5,1,9], val = 5
+输出: [4,1,9]
+解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+示例 2:
+
+输入: head = [4,5,1,9], val = 1
+输出: [4,5,9]
+解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+'''
+
+class DeleteNode:
+    def deleteNode1(self, head: ListNode, val: int) -> ListNode:
+        if not head: return head
+
+        H = ListNode(-1) #伪头结点
+        H.next = head
+        pre, cur = H, H.next
+        while cur.next:
+            if (cur.val == val):
+                pre.next = cur.next  #直接删
+            cur = cur.next
+            pre = pre.next
+
+        if not cur.next:
+            pre.next = None
+
+        return H.next
+
+    #1:
+    def deleteNode2(self, head: ListNode, val: int) -> ListNode:
+        if not head: return head
+
+        H = ListNode(-1) #伪头结点
+        H.next = head
+        pre, cur = H, H.next
+        while cur.next:
+            if (cur.val == val):
+                cur.val = cur.next.val   #用后一个结点的值覆盖当前节点，再删除后一个结点
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+            pre = pre.next
+
+        if not cur.next:
+            pre.next = None
+
+        return H.next
