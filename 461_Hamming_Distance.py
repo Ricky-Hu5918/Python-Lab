255fb8f46b823c17848d9872384d822d4bd839d9
Ricky-Hu5918 <31519963@qq.com>
2020-04-25 15:46:44 +0800

    commit 461_Hamming_Distance.py with xk's version.

--- 461_Hamming_Distance.py
+++ 461_Hamming_Distance.py
@@ -38,6 +38,34 @@ class Solution:
 
         return count
 
+    '''xk's method'''
+    def hammingDistance3(self, x: int, y: int) -> int:
+        if x == y:
+            return 0
+        binx, biny = bin(x)[2:], bin(y)[2:]  #bin()的返回值为字符串‘0bxxxxxx’,因此要去掉前面两个字符
+        maxlen = max(len(binx), len(biny))
+
+        # the first zero padding method
+        binx, biny = '%0*d' % (maxlen, int(binx)), '%0*d' % (maxlen, int(biny))
+
+        # second zero padding method
+        # binx, biny = binx.zfill(maxlen), biny.zfill(maxlen)
+
+        # third (manual) padding method
+        # if len(binx) > len(biny):
+        #     for k in range(len(binx)-len(biny)):
+        #         biny = '0' + biny
+        # elif len(biny) > len(binx):
+        #     for k in range(len(biny)-len(binx)):
+        #         binx = '0' + binx
+
+        count = 0
+        for i in range(maxlen):
+            if binx[i] != biny[i]:
+                count += 1
+
+        return count
+
 x, y = 3, 137
 test = Solution()
 print(test.hammingDistance1(x,y), test.hammingDistance2(x,y))
