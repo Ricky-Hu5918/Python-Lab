7398db12030da86e9de66175a1e5840f42b75b68
Ricky-Hu5918 <31519963@qq.com>
2020-06-22 11:23:45 +0800

    commit 796_Rotate_String.py with 2 versions.

--- 796_Rotate_String.py
+++ 796_Rotate_String.py
@@ -16,9 +16,22 @@ A and B will have length at most 100.
 '''
 '''#思路：只需比较一下两个字符串的长度，然后判断A + A中是否存在B就ok，因为A + A中已经包含了所有可能的移动情况'''
 class Solution:
-    def rotateString(self, A: str, B: str) -> bool:
+    def rotateString1(self, A: str, B: str) -> bool:
         return (B in A*2) if len(A)==len(B) else False
+
+    '''将字符串 A 旋转 s 次后，得到的字符串为 A[s], A[s + 1], A[s + 2], ...，
+    因此我们只要枚举 s，并检查是否有 A[s] == B[0], A[s + 1] == B[1], A[s + 2] == B[2], ... 即可。'''
+    def rotateString2(self, A, B):
+        if len(A) != len(B):
+            return False
+        if len(A) == 0:
+            return True
+
+        for s in range(len(A)):
+            if all(A[(s+i) % len(A)] == B[i] for i in range(len(A))):
+                return True
+        return False
 
 test = Solution()
 A, B = 'abcde', 'cdeab'
-print(test.rotateString(A, B))
+print(test.rotateString1(A, B), test.rotateString2(A,B))
